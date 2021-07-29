cda6a78b6cd9c7b473cf7b9afb621d64ffd1e0ac
virtualpatterns.com <code@virtualpatterns.com>
2021-07-28 21:33:19 -0400

    initial commit

---
+++ source/index.cjs
@@ -0,0 +1,142 @@
+const Is = require('@pwn/is')
+const Path = require('jsonpath')
+
+module.exports = function () {
+
+  let configuration = require('@virtualpatterns/mablung-makefile-environment/babel.json')
+
+  let plugins = Path.query(configuration, '$..plugins')
+
+  plugins.forEach((plugins) => {
+
+    plugins.forEach((plugin, index, plugins) => {
+
+      if (Is.array(plugin)) {
+        // console.log(`plugin[0] = ${plugin[0] == 'importMeta' ? `'${plugin[0]}'` : `require('${plugin[0]}')`}`)
+        plugin[0] = plugin[0] == 'importMeta' ? plugin[0] : require(plugin[0])
+      } else {
+        // console.log(`plugins[${index}] = ${plugin == 'importMeta' ? `'${plugin}'` : `require('${plugin}')`}`)
+        plugins[index] = plugin == 'importMeta' ? plugin : require(plugin)
+      }
+
+    })
+
+  })
+
+  let presets = Path.query(configuration, '$..presets')
+
+  presets.forEach((presets) => {
+
+    presets.forEach((preset, index, presets) => {
+
+      if (Is.array(preset)) {
+        // console.log(`preset[0] = require('${preset[0]}')`)
+        preset[0] = require(preset[0])
+      } else {
+        // console.log(`presets[${index}] = require('${preset}')`)
+        presets[index] = require(preset)
+      }
+
+    })
+
+  })
+
+  return configuration
+
+}
+
+// {
+//   "presets": [
+//     "@virtualpatterns/babel-preset-mablung-makefile"
+//   ],
+//     "env": {
+//     "commonjs": {
+//       "plugins": [
+//         [
+//           "@virtualpatterns/mablung-babel-plugin-replace-identifier",
+//           {
+//             "rule": [
+//               {
+//                 "searchFor": "__filePath",
+//                 "replaceWith": "__filename"
+//               },
+//               {
+//                 "searchFor": "__require",
+//                 "replaceWith": "require"
+//               }
+//             ]
+//           }
+//         ],
+//         [
+//           "@virtualpatterns/mablung-babel-plugin-replace-string-literal",
+//           {
+//             "rule": [
+//               {
+//                 "searchFor": "^(\\.{1,2}\\/.*?)\\.js$",
+//                 "replaceWith": "$1.cjs"
+//               }
+//             ]
+//           }
+//         ]
+//       ],
+//         "presets": [
+//           [
+//             "@babel/preset-env",
+//             {
+//               "targets": {
+//                 "node": "current"
+//               }
+//             }
+//           ]
+//         ]
+//     },
+//     "esmodule": {
+//       "plugins": [
+//         "@babel/plugin-syntax-import-meta",
+//         [
+//           "@virtualpatterns/mablung-babel-plugin-replace-identifier",
+//           {
+//             "rule": [
+//               {
+//                 "searchFor": "__filePath",
+//                 "replaceWith": "__importIdentifier.fileURLToPath(import.meta.url)",
+//                 "parserOption": {
+//                   "plugins": [
+//                     "importMeta"
+//                   ],
+//                   "sourceType": "module"
+//                 },
+//                 "addImport": [
+//                   {
+//                     "type": "default",
+//                     "source": "url",
+//                     "option": {
+//                       "nameHint": "URL"
+//                     }
+//                   }
+//                 ]
+//               },
+//               {
+//                 "searchFor": "__require",
+//                 "replaceWith": "__importIdentifier(import.meta.url)",
+//                 "parserOption": {
+//                   "plugins": [
+//                     "importMeta"
+//                   ],
+//                   "sourceType": "module"
+//                 },
+//                 "addImport": [
+//                   {
+//                     "type": "named",
+//                     "name": "createRequire",
+//                     "source": "module"
+//                   }
+//                 ]
+//               }
+//             ]
+//           }
+//         ]
+//       ]
+//     }
+//   }
+// }
---
+++ source/sandbox/babo.cjs
@@ -0,0 +1,63 @@
+import Is from '@pwn/is'
+import Path from 'jsonpath'
+
+const Configuration = {
+  'plugins': [
+    'a',
+    [
+      'b',
+      {
+        'c': true
+      }
+    ]
+  ],
+  'env': {
+    'commonjs': {
+      'plugins': [
+        'c',
+        [
+          'd',
+          {
+            'e': true
+          }
+        ]
+      ]
+    },
+    'esmodule': {
+      'plugins': [
+        'f',
+        [
+          'g',
+          {
+            'h': true
+          }
+        ]
+      ]
+    }
+  }
+}
+
+let plugins = Path.query(Configuration, '$..plugins')
+
+plugins.forEach((plugins) => {
+
+  plugins.forEach((plugin, index, plugins) => {
+
+    if (Is.array(plugin)) {
+      // console.log(`plugin[0] = '${plugin[0] == 'importMeta' ? plugin[0] : `-${plugin[0]}-`}'`)
+      plugin[0] = plugin[0] == 'importMeta' ? plugin[0] : `-${plugin[0]}-`
+    } else {
+      // console.log(`plugins[${index}] = '${plugin == 'importMeta' ? plugin : `-${plugin}-`}'`)
+      plugins[index] = plugin == 'importMeta' ? plugin : `-${plugin}-`
+    }
+
+  })
+
+})
+
+console.dir(Configuration, { 'depth': null })
+
+// let preset = null
+// preset = Path.query(Configuration, '$..presets[*]')
+// preset = preset.map((preset) => Is.array(preset) ? preset[0] : preset)
+// preset = preset.map((preset) => GetDependencyName(preset))
