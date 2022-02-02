448914d351c183072276b0416383e446dfd9c3e6
virtualpatterns.com <code@virtualpatterns.com>
2022-02-01 22:18:11 -0500

    updates

--- source/library/package.js
+++
@@ -1,6 +0,0 @@
-import FileSystem from 'fs-extra'
-import Path from 'path'
-
-const FolderPath = __folderPath
-
-export const Package = FileSystem.readJsonSync(Path.resolve(FolderPath, '../../package.json'), { 'encoding': 'utf-8' })
