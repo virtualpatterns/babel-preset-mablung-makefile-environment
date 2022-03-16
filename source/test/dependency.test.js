36d077e53c2e4dd666793955662423d99577471b
virtualpatterns.com <code@virtualpatterns.com>
2022-03-16 08:30:02 -0400

    updates

--- source/test/dependency.test.js
+++ source/test/dependency.test.js
@@ -2,11 +2,30 @@ import { Check } from '@virtualpatterns/mablung-check-dependency'
 
 import Test from 'ava'
 
+const Process = process
+
 Test('default', async (test) => {
 
   let dependency = await Check()
 
   test.deepEqual(dependency.missing, {})
   test.deepEqual(dependency.unused, [])
+
+})
+
+Test('section', async (test) => {
+
+  let dependency = await Check(Process.cwd(), {
+    'ignoreMatch': [
+      '@babel/core',
+      '@babel/plugin-syntax-import-meta',
+      '@babel/preset-env',
+      '@virtualpatterns/babel-plugin-mablung-replace-identifier',
+      '@virtualpatterns/babel-plugin-mablung-replace-string-literal',
+      '@virtualpatterns/babel-preset-mablung-makefile'
+    ]
+  })
+
+  test.deepEqual(dependency.section, {})
 
 })
